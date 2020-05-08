e31b3cd9dbd53e7ffaab3b861993af5095585e84
yoyokits <yoyokits@gmail.com>
2020-05-08 03:08:52 +0200

    Set .Net Target Framework version to 4.6.2 to maximize the user scope. Replace FluentAssertion and Ookii dialog wpf with version that have target .net framework version 4.6.2 instead of 4.7.2. Remove unused WebViewControl and WebView that were replaced by cefsharp. Add link to my Twitter and LinkedIn account and add icons.

--- src/YoutubeDlGui/YoutubeDlGui/ViewModels/AboutViewModel.cs
+++ src/YoutubeDlGui/YoutubeDlGui/ViewModels/AboutViewModel.cs
@@ -21,6 +21,8 @@
         {
             this.ApplicationName = AppEnvironment.Name;
             this.ProjectUrlClickedCommand = new RelayCommand(this.OnProjectUrlClicked, nameof(this.ProjectUrlClickedCommand));
+            this.LinkedInCommand = new RelayCommand(this.OnLinkedIn, nameof(this.TwitterCommand));
+            this.TwitterCommand = new RelayCommand(this.OnTwitter, nameof(this.TwitterCommand));
         }
 
         #endregion Constructors
@@ -48,6 +50,11 @@
         public Geometry Icon => Icons.Info;
 
         /// <summary>
+        /// Gets the LinkedInCommand.
+        /// </summary>
+        public ICommand LinkedInCommand { get; }
+
+        /// <summary>
         /// Gets the ProjectUrl.
         /// </summary>
         public string ProjectUrl => AppEnvironment.ProjectUrl;
@@ -56,6 +63,11 @@
         /// Gets the ProjectUrlClickedCommand.
         /// </summary>
         public ICommand ProjectUrlClickedCommand { get; }
+
+        /// <summary>
+        /// Gets the TwitterCommand.
+        /// </summary>
+        public ICommand TwitterCommand { get; }
 
         /// <summary>
         /// Gets the Version.
@@ -67,12 +79,30 @@
         #region Methods
 
         /// <summary>
+        /// The OnLinkedIn.
+        /// </summary>
+        /// <param name="obj">The obj<see cref="object"/>.</param>
+        private void OnLinkedIn(object obj)
+        {
+            Process.Start(@"https://www.linkedin.com/in/ynurcahyo/");
+        }
+
+        /// <summary>
         /// The OnProjectUrlClicked.
         /// </summary>
         /// <param name="obj">The obj<see cref="object"/>.</param>
         private void OnProjectUrlClicked(object obj)
         {
             Process.Start(new ProcessStartInfo(this.ProjectUrl));
+        }
+
+        /// <summary>
+        /// The OnTwitter.
+        /// </summary>
+        /// <param name="obj">The obj<see cref="object"/>.</param>
+        private void OnTwitter(object obj)
+        {
+            Process.Start(@"https://twitter.com/NurcahyoYohanes");
         }
 
         #endregion Methods
